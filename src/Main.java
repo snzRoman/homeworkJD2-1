788cdacf17b46b7c66cccf4e5889939aa7a472e3
RomanValikov <romansbl@bk.ru>
2022-03-24 17:35:45 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Set<Integer> sortedSet = new TreeSet<>();
+        for (int number : intList) {
+            if (number > 0 && number % 2 == 0){
+                sortedSet.add(number);
+            }
+        }
+        System.out.println(sortedSet.toString());
+
+    }
+}
